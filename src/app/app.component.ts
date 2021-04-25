8623338834db2444583a3a76e14ee19a30dc64ed
Eduardo Reyes Garfias <edreygar@gmail.com>
2021-04-25 11:59:33 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'erg-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'historial-clinico';
+}
